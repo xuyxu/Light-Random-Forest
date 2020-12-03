744e3428126336af9c99e74f4e723159f38afdd2
AaronX121 <xuyx@lamda.nju.edu.cn>
2020-12-03 20:12:58 +0800

    Update classification_comparison.py

--- examples/classification_comparison.py
+++ examples/classification_comparison.py
@@ -14,9 +14,8 @@ from sklearn.datasets import load_svmlight_file
 def load_covtype():
     """
     Dataset available at:
-        https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass.html#covtype
+        www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass.html#covtype
     """
-
     # MODIFY THE PATH TO THE DATASET
     data = load_svmlight_file('../../Dataset/LIBSVM/covtype')
     X = data[0].toarray()
@@ -30,21 +29,20 @@ def load_covtype():
 
 if __name__ == "__main__":
 
-    n_estimators = 100
-    verbose = 0
+    n_estimators = 500
     n_jobs = -1
     random_state = 0
 
     X_train, y_train, X_test, y_test = load_covtype()
-    binner = _BinMapper(random_state=random_state)
 
+    # Bin the data is necessary because lightrf only handles data of the
+    # type: `np.uint8`.
+    binner = _BinMapper(random_state=random_state)
     X_train = binner.fit_transform(X_train)
     X_test = binner.transform(X_test)
 
+    # Main body
     model = light_forest(n_estimators=n_estimators,
-                         bootstrap=True,
-                         oob_score=False,
-                         verbose=verbose,
                          n_jobs=n_jobs,
                          random_state=random_state)
 
@@ -61,22 +59,21 @@ if __name__ == "__main__":
     toc = time.time()
     evaluating_time = toc - tic
 
-    print("Light Testing Accuracy: {:.3f} %".format(accuracy_score(y_test, y_pred) * 100.))
+    print("Light Testing Accuracy: {:.3f} %".format(
+        accuracy_score(y_test, y_pred) * 100.))
     print("Model Size: {:.3f} MB".format(size / (1024 * 1024)))
     print("Training Time: {:.3f} s".format(training_time))
     print("Evaluating Time: {:.3f} s\n".format(evaluating_time))
 
     model = sklearn_forest(n_estimators=n_estimators,
-                           bootstrap=True,
-                           verbose=verbose,
                            n_jobs=n_jobs,
                            random_state=random_state)
-    
+
     tic = time.time()
     model.fit(X_train, y_train)
     toc = time.time()
     training_time = toc - tic
-    
+
     handler = pickle.dumps(model)
     size = sys.getsizeof(handler)
 
@@ -85,7 +82,8 @@ if __name__ == "__main__":
     toc = time.time()
     evaluating_time = toc - tic
 
-    print("Sklearn Testing Accuracy: {:.3f} %".format(accuracy_score(y_test, y_pred) * 100.))
+    print("Sklearn Testing Accuracy: {:.3f} %".format(
+        accuracy_score(y_test, y_pred) * 100.))
     print("Model Size: {:.3f} MB".format(size / (1024 * 1024)))
     print("Training Time: {:.3f} s".format(training_time))
     print("Evaluating Time: {:.3f} s\n".format(evaluating_time))
